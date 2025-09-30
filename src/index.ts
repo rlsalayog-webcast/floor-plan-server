d63df244918bb9a11e93a395dd60ad981c755f7f
Raphael Earl Salayog <rlsalayog@findme.com.ph>
2025-10-01 00:06:29 +0800

    feat: setup database

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@as-integrations/express5";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@as-integrations/express5";
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import http from "http";
-import cors from "cors";
-import { typeDefs } from "./schema";
+import { connectDB } from "../utils/database";
 import { resolvers } from "./resolver";
+import { typeDefs } from "./schema";
 
 interface MyContext {
     token?: string;
 }
 
-// Required logic for integrating with Express
+dotenv.config();
+
 const app = express();
-// Our httpServer handles incoming requests to our Express app.
-// Below, we tell Apollo Server to "drain" this httpServer,
-// enabling our servers to shut down gracefully.
 const httpServer = http.createServer(app);
 
 const server = new ApolloServer<MyContext>({
@@ -23,21 +23,18 @@ const server = new ApolloServer<MyContext>({
     resolvers,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
-// Ensure we wait for our server to start
+
 await server.start();
 
-// Set up our Express middleware to handle CORS, body parsing,
-// and our expressMiddleware function.
 app.use(
     "/",
     cors<cors.CorsRequest>(),
     express.json(),
-    // expressMiddleware accepts the same arguments:
-    // an Apollo Server instance and optional configuration options
     expressMiddleware(server, {
         context: async ({ req }) => ({ token: req.headers.token }),
     })
 );
-
-await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000/`);
+connectDB(async () => {
+    await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000/`);
+});
