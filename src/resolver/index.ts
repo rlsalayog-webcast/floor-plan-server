52ab439294bfa301434a77da30ba81c60413f918
Raphael Earl Salayog <rlsalayog@findme.com.ph>
2025-09-30 23:23:48 +0800

    feat: typeDef

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,16 +1,19 @@
 // Resolvers define how to fetch the types defined in your schema.
+
+import GraphQLJSON from "graphql-type-json";
+
 // This resolver retrieves books from the "books" array above.
 export const resolvers = {
+    JSON: GraphQLJSON, // 👈 hook up the custom scalar
     Query: {
-        books: () => [
-            {
-                title: "The Awakening",
-                author: "Kate Chopin",
-            },
-            {
-                title: "City of Glass",
-                author: "Paul Auster",
-            },
+        getLandmarks: () => [
+            /* return your data */
         ],
+        getLandmarkById: (_, { id }) => {
+            /* return one */
+        },
+        getFloorByLevel: (_, { landmarkId, level }) => {
+            /* return floor */
+        },
     },
 };
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,22 +1,59 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 export const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+    scalar JSON
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+    type Query {
+        getLandmarks: [Landmark!]!
+        getLandmarkById(id: ID!): Landmark
+        getFloorByLevel(landmarkId: ID!, level: String!): Floor
+    }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
+    type LandmarkCoordinates {
+        latitude: Float!
+        longitude: Float!
+    }
+
+    type FloorCoordinates {
+        x: Float!
+        y: Float!
+    }
+
+    type Location {
+        address: String!
+        city: String!
+        country: String!
+        coordinates: LandmarkCoordinates!
+    }
+
+    type FloorPlanElementAttributes {
+        name: String!
+        description: String!
+    }
+
+    type FloorPlanElement {
+        id: ID!
+        coordinates: FloorCoordinates!
+        width: Float
+        height: Float
+        backgroundColor: String!
+        textColor: String!
+        attributes: FloorPlanElementAttributes!
+    }
+
+    type Floor {
+        id: ID!
+        level: String!
+        name: String!
+        description: String
+        elements: [FloorPlanElement!]!
+    }
+
+    type Landmark {
+        id: ID!
+        name: String!
+        location: Location!
+        description: String
+        floor_plans: [Floor!]!
+        created_at: String!
+        updated_at: String!
+    }
 `;
